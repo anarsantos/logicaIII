b44ecd7f2479dee9c7d02e6fb740bab2a7d3039d
anarsantos <aninha_rsantos@yahoo.com.br>
2024-08-05 16:37:12 -0300

    estrutura condicional switch case

---
+++ src/EstruturaEscolha/EstruturaEscolha.java
@@ -0,0 +1,17 @@
+package EstruturaEscolha;
+
+public class EstruturaEscolha {
+    public static void main(String[] args) {
+        int codigoProduto = 2;
+
+        switch (codigoProduto) {
+            case 1:
+                System.out.println("Produto 1");
+                break;
+            case 2:
+                System.out.println("Produto 2");
+                break;
+            default:
+        }
+    }
+}
