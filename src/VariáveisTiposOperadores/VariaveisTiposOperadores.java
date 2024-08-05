edde09e5fa1d786c4e1e0177ff0771b179d0c7cb
anarsantos <aninha_rsantos@yahoo.com.br>
2024-08-05 15:44:51 -0300

    variáveis primitivas

---
+++ src/VariáveisTiposOperadores/VariaveisTiposOperadores.java
@@ -0,0 +1,14 @@
+package VariáveisTiposOperadores;
+
+public class VariaveisTiposOperadores {
+    public static void main(String[] args) {
+        int idade = 0;
+        float cotacaoDolar = 5.0f;
+        double cotacaoEuro = 6.0d;
+        char genero = 'M';
+        byte ponto = 0;
+        boolean estaCadastrado = false;
+
+        String nome = "Texto de variável String";
+    }
+}
