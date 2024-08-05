85b52fd1b31435e48626128ef608fb7d339f92f4
anarsantos <aninha_rsantos@yahoo.com.br>
2024-08-05 16:21:13 -0300

    estrutura condicional if else

---
+++ src/EstruturaCondicionalSe/EstruturaCondicionalSe.java
@@ -0,0 +1,17 @@
+package EstruturaCondicionalSe;
+
+public class EstruturaCondicionalSe {
+    public static void main(String[] args) {
+        int media = 7;
+
+        if(media >= 7) {
+            if(media == 10) {
+                //aprovado com nota máxima
+            } else {
+                //aprovado
+            }
+        } else {
+            //reprovado
+        }
+    }
+}
