fe958eb4e8c6cc3e0dfda1f5f148c32499a6aae1
anarsantos <aninha_rsantos@yahoo.com.br>
2024-08-05 15:13:54 -0300

    entrada e saída

--- src/PrimeiroProjeto/PrimeiroProjeto.java
+++ src/PrimeiroProjeto/PrimeiroProjeto.java
@@ -1,7 +1,23 @@
 package PrimeiroProjeto;
 
+import java.util.Scanner;
+
 public class PrimeiroProjeto {
     public static void main(String[] args) {
         System.out.println("Olá mundo!");
+
+        Scanner leitor = new Scanner(System.in);
+
+        //entrada de dados
+        int idade = leitor.nextInt();
+        float cotacaoDolar = leitor.nextFloat();
+        double cotacaoEuro = leitor.nextDouble();
+        String nome = leitor.nextLine();
+        String codigoRua = leitor.next();
+
+        //saída de dados
+        System.out.println("Idade: "); //pula uma linha
+        System.out.print("Dólar: "); //não pula linha
+
     }
 }
