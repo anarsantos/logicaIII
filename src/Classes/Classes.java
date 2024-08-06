7b7bde43afa113993167056d49df3a5441b9bf8a
anarsantos <aninha_rsantos@yahoo.com.br>
2024-08-06 17:03:41 -0300

    criação do objeto

--- src/Classes/Classes.java
+++ src/Classes/Classes.java
@@ -1,7 +1,19 @@
 package Classes;
 
+import java.util.Scanner;
+
 public class Classes {
     public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
 
+        //no lugar de peso e altura, criar a variável pessoa (não primitiva).
+        Pessoa objetoPessoa = new Pessoa();
+
+        System.out.print("Digite o peso da pessoa: ");
+        objetoPessoa.peso = leitor.nextFloat();
+        System.out.print("Digite a altura da pessoa: ");
+        objetoPessoa.altura = leitor.nextFloat();
+
+        System.out.println("IMC = " + objetoPessoa.calcularIMC());
     }
 }
